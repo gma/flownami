89ce3ab60ab94f46fc137bf151195b5f8d8d2bd2
Graham Ashton <graham@effectif.com>
2025-02-13 20:12:27 +0000

    Start laying out the board
    This is as far as we got while mobbing as a group on 13 Feb 2025.

--- main.ts
+++ main.ts
@@ -9,6 +9,10 @@ app.get("/", function (_, res) {
   res.render("pages/index");
 });
 
+app.get("/board", function (req, res) {
+  res.render("pages/board");
+});
+
 const port = Deno.env.get("PORT") || 8080;
 app.listen(port);
 console.log(`Server is listening on port ${port}`);
